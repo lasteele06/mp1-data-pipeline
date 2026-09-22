ca16e4065262bd3a937914e82b4890ef4677ef00
lasteele06 <thirdsonsteele@gmail.com>
2026-09-22 13:08:35 -0400

    corrections to if

--- pipeline.py
+++ pipeline.py
@@ -1,21 +1,23 @@
 import argparse
 import logging
 import sys
+from unicodedata import name
+import verbose
 from pathlib import Path
 
 logger = logging.getLogger(name)
 
 def setup_logging(verbose=False):
-if verbose:
-    level= logging.DEBUG
-else:
-    level= logging.INFO
+    if verbose:
+        level= logging.DEBUG
+    else:
+        level= logging.INFO
 
     logging.basicConfig(
-        level=level
+        level=level,
         format = "%(asctime)s %(levelname)s  %(message)s", 
         datefmt="%H:%M:%S"
-    )
+     )
     pass
 
 def parse_arguments():
